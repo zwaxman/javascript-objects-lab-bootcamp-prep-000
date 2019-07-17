3f81398ba4287e5895258f4bc314a34511845ea2
Zachary M Waxman <zwaxman@gmail.com>
2019-07-17 00:03:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ function updateObjectWithKeyAndValue(obj,key,value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(obj,key,value){
-  return obj[key]=value;
+  obj[key]=value;
+  return obj
 }
 
 //   describe('destructivelyUpdateObjectWithKeyAndValue(object, key, value)', function() {
